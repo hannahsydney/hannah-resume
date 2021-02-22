2718f6f5371c8f34339e46c1f793adf09a9da188
Hannah Chen <hannah.chen1109@gmail.com>
2021-02-22 13:17:25 +1100

    resumedata url update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
 	getResumeData() {
 		$.ajax({
-			url: '/resumeData.json',
+			url: '/hannah-resume/resumeData.json',
 			dataType: 'json',
 			cache: false,
 			success: function(data) {
