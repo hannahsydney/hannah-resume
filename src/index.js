1f87afcfe703e120136e533ef933233068f41b44
Hannah Chen <hannah.chen1109@gmail.com>
2021-02-22 12:59:24 +1100

    unregister

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ServiceWorker.unregister();
+// ServiceWorker.unregister();
 // registerServiceWorker();
+unregister();
