52eb16dd8e92cc0f6266350e6df3d067a9a8e31f
Hannah Chen <hannah.chen1109@gmail.com>
2021-02-23 22:38:20 +1100

    built using react

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,12 +22,7 @@ class Footer extends Component {
 
 						<ul className="copyright">
 							<li>&copy; Copyright 2021 Hannah Chen</li>
-							<li>
-								Design by{' '}
-								<a title="Styleshout" href="http://www.styleshout.com/">
-									Styleshout
-								</a>
-							</li>
+							<li>Built using React</li>
 						</ul>
 					</div>
 					<div id="go-top">
