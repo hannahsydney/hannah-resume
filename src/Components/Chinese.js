3b3e8d409860017e80b98ed287eb57b1357a36a2
hannahsydney <hannah.chen1109@gmail.com>
2024-11-07 21:16:36 +1100

    2024 update

--- src/Components/Chinese.js
+++ src/Components/Chinese.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Chinese extends Component {
-	render() {
+	render () {
 		return (
 			<section id="testimonials">
 				<div className="text-container">
@@ -17,19 +17,12 @@ class Chinese extends Component {
 							<br />
 							<blockquote>
 								<p>
-									我是华裔澳洲人，在澳洲出生、长大。所以我一直对普通话和中国语言学感兴趣。我目前通过网上教学的课程{' '}
-									<a href="https://www.culturestride.com" style={{ color: 'inherit' }}>
-										CultureStride
-									</a>{' '}
-									学习普通话。目前我已经在 UNSW 完成了非母语课程的中文初级班，并且获得 92 分的优等成绩。
+									我是华裔澳洲人，在澳洲出生、长大。所以我一直对中国文化和语言感兴趣。我目前在 Sydney HSK Centre (HSK 5)
+									学习普通话。我已经在 UNSW 完成了非母语课程的中文初级班，并且获得 92 分的优等成绩。
 								</p>
 								<p>
 									Growing up as an Australian with a Chinese heritage, I have always been interested
-									in Mandarin and liguistics. I'm currently studying Mandarin Chinese with the Chinese
-									language school,{' '}
-									<a href="https://www.elitelinguistic.com/chinese-lesson-free-trial/ref/eln/?campaign=GAds&gclid=CjwKCAjwh4ObBhAzEiwAHzZYUxsYYOjpIAYOJ637Clh9BUJNh0-3nDkixoK23yTibZh8Mm5TyAa2QhoCUS8QAvD_BwE" style={{ color: 'inherit' }}>
-										Elite Linguistic Network Chinese Language School
-									</a>, here in Singapore. I have also completed Introductory Chinese for Background Speakers at UNSW,
+									in Mandarin and liguistics. I'm currently studying Mandarin Chinese at the Sydney HSK Centre (HSK 5) I have also completed Introductory Chinese for Background Speakers at UNSW,
 									achieving a final grade of 92.
 								</p>
 							</blockquote>
